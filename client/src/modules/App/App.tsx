abe51865c4a670fb987b6760bd7ceebf2624e35b
Ryś <ryszard@MacBook-Pro-Rys.local>
2020-07-06 14:55:19 +0200

    Initial project configuration

---
+++ client/src/modules/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import GlobalStyle from "../shared/UI/GlobalStyle";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      _APP_
+    </div>
+  );
+}
+
+export default App;
