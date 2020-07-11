5ac1a877ccec466aa4cbe9dd4bf3cecee131c653
Ryszard Plewnia <rplewnia@gmail.com>
2020-07-11 17:51:43 +0200

    Display technologies icons

--- client/src/modules/Home/Technologies/index.tsx
+++ client/src/modules/Home/Technologies/index.tsx
@@ -5,6 +5,11 @@ import { SectionWrapper } from "src/modules/Home/shared/components";
 import Heading from "src/modules/shared/UI/Heading";
 import P from "src/modules/shared/UI/Paragraph";
 import { TranslationKey } from "src/modules/Translations/Translation";
+import {
+  IconsWrapper,
+  IconWithFlex,
+  IconWrapper,
+} from "src/modules/Home/Technologies/components";
 
 const Technologies: FC = () => {
   const { t } = useTranslation();
@@ -13,6 +18,20 @@ const Technologies: FC = () => {
     <SectionWrapper>
       <Heading>{t(TranslationKey.TECHNOLOGIES)}</Heading>
       <P>{t(TranslationKey.TECHNOLOGIES_SHORT_DESCRIPTION)}</P>
+      <IconsWrapper>
+        <IconWrapper>
+          <IconWithFlex type="React" />
+        </IconWrapper>
+        <IconWrapper>
+          <IconWithFlex type="Node" />
+        </IconWrapper>
+        <IconWrapper>
+          <IconWithFlex type="GraphQL" />
+        </IconWrapper>
+        <IconWrapper>
+          <IconWithFlex type="Docker" />
+        </IconWrapper>
+      </IconsWrapper>
     </SectionWrapper>
   );
 };
