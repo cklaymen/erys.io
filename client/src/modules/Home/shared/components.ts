5929d29a65012af2e23d990ed72ce37ff46688ce
Ryszard Plewnia <rplewnia@gmail.com>
2020-07-14 16:29:26 +0200

    Add more space between sections on larger devices

--- client/src/modules/Home/shared/components.ts
+++ client/src/modules/Home/shared/components.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import media from "src/modules/shared/UI/media";
+
 export const SectionWrapper = styled.section`
   margin-top: 40px;
+  ${media.medium`
+    margin-top: 80px;
+  `}
 `;
