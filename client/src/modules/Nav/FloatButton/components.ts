854fa78964cf6d1c23904dc0d6d33d72abfcb063
Ryszard Plewnia <rplewnia@gmail.com>
2020-07-20 16:23:21 +0200

    Create FloatButton

---
+++ client/src/modules/Nav/FloatButton/components.ts
@@ -0,0 +1,40 @@
+import styled, { css } from "styled-components";
+
+import Button, {
+  buttonHoverStyle,
+  buttonActiveStyle,
+  buttonFocusStyle,
+} from "src/modules/shared/UI/Button";
+import Icon from "src/modules/shared/UI/Icon";
+
+export const FLOAT_BUTTON_SIZE_EXTRA_SMALL = "60px";
+
+export const floatButtonHoverStyle = buttonHoverStyle;
+
+export const floatButtonFocusStyle = buttonFocusStyle;
+
+export const floatButtonActiveStyle = () => css`
+  ${buttonActiveStyle()};
+  opacity: initial;
+`;
+
+export const FloatButtonWrapper = styled(Button)`
+  width: ${FLOAT_BUTTON_SIZE_EXTRA_SMALL};
+  height: ${FLOAT_BUTTON_SIZE_EXTRA_SMALL};
+  border-radius: 50%;
+  padding: 15px;
+
+  &:hover {
+    ${floatButtonHoverStyle()};
+  }
+  &:focus {
+    ${floatButtonFocusStyle()};
+  }
+  &:active {
+    ${floatButtonActiveStyle()};
+  }
+`;
+
+export const FloatButtonIcon = styled(Icon)`
+  width: 100%;
+`;
