b3d5ccb3cad207882146c9f95633b64de0bb13b8
Ryszard Plewnia <rplewnia@gmail.com>
2020-07-22 17:40:39 +0200

    Add consts

---
+++ client/src/const.ts
@@ -0,0 +1,7 @@
+export const PHONE_VALUE = "+48669712600";
+export const PHONE = "669 712 600";
+export const EMAIL = "hello@erys.io";
+export const FB_URL = "https://fb.com/erys.ioo";
+export const INSTAGRAM_URL = "https://instagram.com/erys.io";
+export const LINKEDIN_URL = "https://linkedin.com/in/ryszard-plewnia";
+export const COMPANY_NAME = "Aplikacje webowe ERYS' Ryszard Plewnia";
