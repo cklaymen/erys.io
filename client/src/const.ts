160b7e4732bcb7db92d4be5c2671f368d72bb099
Ryszard Plewnia <rplewnia@gmail.com>
2020-07-23 10:15:51 +0200

    Correct phone number

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,5 +1,5 @@
-export const PHONE_VALUE = "+48669712600";
-export const PHONE = "669 712 600";
+export const PHONE_VALUE = "+48699712600";
+export const PHONE = "699 712 600";
 export const EMAIL = "hello@erys.io";
 export const FB_URL = "https://fb.com/erys.ioo";
 export const INSTAGRAM_URL = "https://instagram.com/erys.io";
