f07bde6ecc1b1e49dc176a6a0d95421b8e881313
Ryszard Plewnia <rplewnia@gmail.com>
2020-07-27 15:34:29 +0200

    Move SectionWrapper to shared

--- client/src/modules/Home/Technologies/index.tsx
+++ client/src/modules/Home/Technologies/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
 
-import { SectionWrapper } from "src/modules/Home/shared/components";
 import Heading from "src/modules/shared/UI/Heading";
 import P from "src/modules/shared/UI/Paragraph";
 import { TranslationKey } from "src/modules/Translations/Translation";
@@ -10,12 +9,13 @@ import {
   IconWithFlex,
   IconWrapper,
 } from "src/modules/Home/Technologies/components";
+import Section from "src/modules/shared/UI/Section";
 
 const Technologies: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <SectionWrapper>
+    <Section>
       <Heading>{t(TranslationKey.TECHNOLOGIES)}</Heading>
       <P>{t(TranslationKey.TECHNOLOGIES_SHORT_DESCRIPTION)}</P>
       <IconsWrapper>
@@ -32,7 +32,7 @@ const Technologies: FC = () => {
           <IconWithFlex type="Docker" />
         </IconWrapper>
       </IconsWrapper>
-    </SectionWrapper>
+    </Section>
   );
 };
 
