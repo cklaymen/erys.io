d8d916316c3372dfa0a9da87658f51ce2fb6aa69
Ryszard Plewnia <rplewnia@gmail.com>
2020-08-05 14:46:10 +0200

    Policy cookies content

--- client/src/modules/Cookies/Main.components.tsx
+++ client/src/modules/Cookies/Main.components.tsx
@@ -1,3 +1,10 @@
 import styled from "styled-components";
+import { defaultTextLink } from "src/modules/shared/UI/defaults";
+import ExternalLink from "src/modules/shared/ExternalLink";
 
 export const CookiesMainWrapper = styled.div``;
+
+export const Link = styled(ExternalLink)`
+  ${defaultTextLink()}
+  word-break: break-word;
+`;
