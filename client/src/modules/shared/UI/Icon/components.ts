a3be39ff72df1e404f163931f120375df3938ce6
Ryszard Plewnia <rplewnia@gmail.com>
2020-10-02 13:36:16 +0200

    Add Blog icon

--- client/src/modules/shared/UI/Icon/components.ts
+++ client/src/modules/shared/UI/Icon/components.ts
@@ -2,10 +2,6 @@ import styled from "styled-components";
 
 export const IconWrapper = styled.span`
   display: inline-block;
-  & path,
-  & rect {
-    fill: currentColor;
-  }
   & svg {
     width: 100%;
     height: 100%;
